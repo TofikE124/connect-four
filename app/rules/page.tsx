a2efe391c390ca6d8c8827d6b222d23538fb2a6a
TofikE124 <eleltofik@gmail.com>
2024-08-04 22:29:26 +0300

    Fixing rules alignment issue

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -12,50 +12,48 @@ export const metadata: Metadata = {
 
 const page = () => {
   return (
-    <div className="w-full h-screen bg-purple">
-      <div className="size-full grid place-items-center">
-        <div className="bg-white lgmd:max-w-[480px] sm:mx-[5vw] sm:max-w-[340px] relative space-y-7 pt-[30px] lgmd:pb-[56px] sm:pb-20 lgmd:px-[34px] sm:px-5 rounded-[40px] shadow-[0px_10px_0px_0px_black] border-[3px] border-solid border-black">
-          <h2 className="heading-l text-black uppercase text-center">Rules</h2>
-          <div className="space-y-4">
-            <h4 className="heading-s text-purple uppercase">Objective</h4>
-            <p className="text-black">
-              Be the first player to connect 4 of the same colored discs in a
-              row (either vertically, horizontally, or diagonally).
-            </p>
-          </div>
-          <div className="space-y-4">
-            <h4 className="heading-s text-purple uppercase">How To Play</h4>
-            <ol className="space-y-3 list-decimal list-inside ">
-              <li>
-                <span className="opacity-60">
-                  Red goes first in the first game.
-                </span>
-              </li>
-              <li>
-                <span className="opacity-60">
-                  Players must alternate turns, and only one disc can be dropped
-                  in each turn.
-                </span>
-              </li>
-              <li>
-                <span className="opacity-60">
-                  The game ends when there is a 4-in-a-row or a stalemate.
-                </span>
-              </li>
-              <li>
-                <span className="opacity-60">
-                  The starter of the previous game goes second on the next game.
-                </span>
-              </li>
-            </ol>
-          </div>
-          <Link
-            href="/"
-            className="absolute bottom-0 translate-y-[50%] left-[50%] translate-x-[-50%]"
-          >
-            <Button variant="icon" color="red" icon={CheckIcon}></Button>
-          </Link>
+    <div className="w-screen h-screen grid place-items-center bg-purple sm:py-4 overflow-y-scroll">
+      <div className="bg-white h-fit lgmd:max-w-[480px] sm:mx-[5vw] sm:max-w-[340px] relative space-y-7 pt-[30px] lgmd:pb-[56px] sm:pb-20 lgmd:px-[34px] sm:px-5 rounded-[40px] shadow-[0px_10px_0px_0px_black] border-[3px] border-solid border-black mb-[50px]">
+        <h2 className="heading-l text-black uppercase text-center">Rules</h2>
+        <div className="space-y-4">
+          <h4 className="heading-s text-purple uppercase">Objective</h4>
+          <p className="text-black">
+            Be the first player to connect 4 of the same colored discs in a row
+            (either vertically, horizontally, or diagonally).
+          </p>
         </div>
+        <div className="space-y-4">
+          <h4 className="heading-s text-purple uppercase">How To Play</h4>
+          <ol className="space-y-3 list-decimal list-inside ">
+            <li>
+              <span className="opacity-60">
+                Red goes first in the first game.
+              </span>
+            </li>
+            <li>
+              <span className="opacity-60">
+                Players must alternate turns, and only one disc can be dropped
+                in each turn.
+              </span>
+            </li>
+            <li>
+              <span className="opacity-60">
+                The game ends when there is a 4-in-a-row or a stalemate.
+              </span>
+            </li>
+            <li>
+              <span className="opacity-60">
+                The starter of the previous game goes second on the next game.
+              </span>
+            </li>
+          </ol>
+        </div>
+        <Link
+          href="/"
+          className="absolute bottom-0 translate-y-[50%] left-[50%] translate-x-[-50%]"
+        >
+          <Button variant="icon" color="red" icon={CheckIcon}></Button>
+        </Link>
       </div>
     </div>
   );
